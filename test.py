be61cba76628b30b30a6decb14d2f37ae1879ee4
Jônatas <Jnatas>
2019-06-21 15:04:41 -0300

    update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from streamdownload import downloadFile
 
 
-print(downloadFile('https://images.pexels.com/photos/2387869/pexels-photo-2387869.jpeg', local='./downloadedfile.png', progress=True))
+downloadFile('https://images.pexels.com/photos/2387869/pexels-photo-2387869.jpeg', local='./downloadedfile.png', progress=True)
